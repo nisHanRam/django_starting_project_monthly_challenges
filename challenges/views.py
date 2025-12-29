da6ad84cdad3b9d55c25cecbabdb786ad0dc20dc
Nishant Choubisa <nishant.choubisa@arcgate.com>
2025-12-29 09:44:04 +0530

    rendering templates

--- challenges/views.py
+++ challenges/views.py
@@ -43,7 +43,6 @@ def monthly_challenge_by_number(request, month):
 
 def monthly_challenge(request, month):
     try:
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
