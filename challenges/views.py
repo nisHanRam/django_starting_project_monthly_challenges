d231754f98c031679660cc52e2f752f90fa9a582
Nishant Choubisa <nishant.choubisa@arcgate.com>
2025-12-29 17:54:30 +0530

    404 template

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from functools import reduce
+# from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for entire month",
@@ -46,4 +46,14 @@ def monthly_challenge(request, month):
             {"text": challenge_text, "month": month},
         )
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        # Approach-1 - Manually returning a 404 page
+        # We can't use render function here because it always returns a success response
+        # Therefore, we need to import and use render_to_string function
+        # response_data = render_to_string("404.html")
+        # return HttpResponseNotFound(response_data)
+    
+        # Approach-2 - Raising the error class Http404
+        # This automatically picks a file named "404.html" from templates folder at the root
+        # In dev mode it shows error as debug is True & in production mode it renders 404.html
+        raise Http404() 
+
